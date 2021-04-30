244efb7c6addf7c2c0516212d2ee0097bab70936
eugen <garret.evg@gmail.com>
2021-04-30 13:40:02 +0300

    add logger

---
+++ ButtonUtil/src/main/java/ua/eugen/buttonutil/Logger.kt
@@ -0,0 +1,9 @@
+package ua.eugen.buttonutil
+
+import android.util.Log
+
+class Logger {
+    fun log(mess: String){
+        Log.d("log_tag",mess)
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+[![](https://jitpack.io/v/garret1evg/ButtonUtil-Android.svg)](https://jitpack.io/#garret1evg/ButtonUtil-Android)
